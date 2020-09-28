f1dbab7bcf54d20f57c9bb9a1a71f01fd14ab4cc
MingcongLi-casual <limc17@tsinghua.org.cn>
2020-09-28 23:53:05 +0800

    remove git repo and put that in setup_ros

--- setup_ros.sh
+++ setup_ros.sh
@@ -1,5 +1,8 @@
+cd ./setup_materials/
+git clone https://github.com/ncnynl/installROSXavier
+cd ../
 cd ./setup_materials/installROSXavier
 ./installROS.sh -p ros-melodic-desktop
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-
+cd ../
